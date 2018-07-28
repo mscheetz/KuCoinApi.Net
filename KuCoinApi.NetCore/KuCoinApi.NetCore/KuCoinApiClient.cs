b69fb9525f7ef636faa408acf6a6e3a081cd198b
Matt Scheetz <mfscheetz@gmail.com>
2018-07-27 20:19:13 -0400

    tests and client

---
+++ KuCoinApi.NetCore/KuCoinApi.NetCore/KuCoinApiClient.cs
@@ -0,0 +1,29 @@
+﻿using KuCoinApi.NetCore.Data;
+using KuCoinApi.NetCore.Data.Interface;
+using System;
+
+namespace KuCoinApi.NetCore
+{
+    public class KuCoinApiClient
+    {
+        public IKuCoinRepository KuCoinRepository;
+
+        /// <summary>
+        /// Constructor, no authorization
+        /// </summary>
+        public KuCoinApiClient()
+        {
+            KuCoinRepository = new KuCoinRepository();
+        }
+
+        /// <summary>
+        /// Constructor, with authorization
+        /// </summary>
+        /// <param name="apiKey">Api key</param>
+        /// <param name="apiSecret">Api secret</param>
+        public KuCoinApiClient(string apiKey, string apiSecret)
+        {
+            KuCoinRepository = new KuCoinRepository(apiKey, apiSecret);
+        }
+    }
+}
---
+++ KuCoinApi.NetCore/KuCoinApi.NetCore/Tests/KuCoinRepositoryTests.cs
@@ -0,0 +1,151 @@
+﻿using KuCoinApi.NetCore.Entities;
+using KuCoinApi.NetCore.Data.Interface;
+using System;
+using System.Linq;
+using Xunit;
+using KuCoinApi.NetCore.Data;
+
+namespace KuCoinApi.NetCore.Tests
+{
+    public class KuCoinRepositoryTests : IDisposable
+    {
+
+        private string apiKey = "Enter your key";
+        private string apiSecret = "Enter your secret";
+
+        public KuCoinRepositoryTests()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void GetCandlesticksTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var interval = Interval.FifteenM;
+            var symbol = "ETH-BTC";
+
+            var sticks = repo.GetCandlesticks(symbol, interval, 10).Result;
+
+            Assert.True(sticks != null);
+            Assert.True(sticks.close.Length > 0);
+            Assert.True(sticks.open.Length > 0);
+        }
+
+        [Fact]
+        public void GetAccountBalanceTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+
+            var balances = repo.GetBalance().Result;
+
+            Assert.True(balances != null);
+        }
+
+        [Fact]
+        public void GetOrderBookTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var symbol = "ETH-BTC";
+
+            var orderBook = repo.GetOrderBook(symbol).Result;
+
+            Assert.True(orderBook != null);
+            Assert.True(orderBook.buys.Length > 0);
+            Assert.True(orderBook.sells.Length > 0);
+        }
+
+        [Fact]
+        public void GetTicksTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+
+            var ticks = repo.GetTicks().Result;
+
+            Assert.True(ticks != null);
+        }
+
+        [Fact]
+        public void GetTickTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var symbol = "ETH-BTC";
+
+            var tick = repo.GetTick(symbol).Result;
+
+            Assert.True(tick != null);
+        }
+
+        [Fact]
+        public void GetOrdersTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var symbol = "DCC-BTC";
+
+            var orders = repo.GetOrders(symbol).Result;
+
+            Assert.True(orders != null);
+        }
+
+        [Fact]
+        public void GetOpenOrdersTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var symbol = "DCC-BTC";
+
+            var orders = repo.GetOpenOrders(symbol).Result;
+
+            Assert.True(orders != null);
+        }
+
+        [Fact]
+        public void PostTradeTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var symbol = "DCC-BTC";
+            var tradeParams = new TradeParams
+            {
+                price = 0.00000400M,
+                quantity = 3000,
+                symbol = symbol,
+                type = "BUY"
+            };
+
+            var orderDetail = repo.PostTrade(tradeParams).Result;
+
+            Assert.True(orderDetail != null);
+        }
+
+        [Fact]
+        public void GetAndCancelOpenTradeTest()
+        {
+            IKuCoinRepository repo = new KuCoinRepository(apiKey, apiSecret);
+            var symbol = "DCC-BTC";
+
+            var orders = repo.GetOpenOrders(symbol).Result;
+
+            Assert.True(orders != null);
+
+            if (orders.openBuys.Length > 0)
+            {
+                var orderId = orders.openBuys[0].orderId;
+
+                var cancelDetail = repo.DeleteTrade(symbol, orderId, orders.openBuys[0].type).Result;
+
+                Assert.True(cancelDetail != null);
+            }
+
+            if(orders.openSells.Length > 0)
+            {
+                var orderId = orders.openSells[0].orderId;
+
+                var cancelDetail = repo.DeleteTrade(symbol, orderId, orders.openSells[0].type).Result;
+
+                Assert.True(cancelDetail != null);
+            }
+        }
+    }
+}
