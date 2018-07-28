6e9ea3409a05d2897a8a0d95a76fbf2859bd2bbf
Matt Scheetz <mfscheetz@gmail.com>
2018-07-27 20:18:56 -0400

    kucoin repository and interface

---
+++ KuCoinApi.NetCore/KuCoinApi.NetCore/Data.Interface/IKuCoinRepository.cs
@@ -0,0 +1,100 @@
+﻿using KuCoinApi.NetCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KuCoinApi.NetCore.Data.Interface
+{
+    public interface IKuCoinRepository
+    {
+        /// <summary>
+        /// Check if the Exchange Repository is ready for trading
+        /// </summary>
+        /// <returns>Boolean of validation</returns>
+        bool ValidateExchangeConfigured();
+        
+        /// <summary>
+        /// Get candlesticks
+        /// </summary>
+        /// <param name="symbol">String of symbol</param>
+        /// <param name="size">stick size</param>
+        /// <param name="limit">number of sticks</param>
+        /// <returns>ChartValue object</returns>
+        Task<ChartValue> GetCandlesticks(string symbol, Interval size, int limit);
+
+        /// <summary>
+        /// Get account balance
+        /// </summary>
+        /// <returns>Balance array</returns>
+        Task<Balance[]> GetBalance();
+
+        /// <summary>
+        /// Get order information
+        /// </summary>
+        /// <param name="symbol">string of symbol</param>
+        /// <param name="tradeType">Trade type</param>
+        /// <param name="orderId">long of orderId</param>
+        /// <param name="page">Page number, default 1</param>
+        /// <param name="limit">Number of fills to return, default 20</param>
+        /// <returns>OrderResponse object</returns>
+        Task<OrderListDetail> GetOrder(string symbol, TradeType tradeType, long orderId, int page = 1, int limit = 20);
+
+        /// <summary>
+        /// Get all current user order information
+        /// </summary>
+        /// <param name="symbol">string of symbol</param>
+        /// <param name="limit">Int of orders count to return, default 20</param>
+        /// <param name="page">Int of page number</param>
+        /// <returns>OpenOrderResponse object</returns>
+        Task<OrderListDetail[]> GetOrders(string symbol, int limit = 20, int page = 1);
+
+        /// <summary>
+        /// Get all open orders
+        /// </summary>
+        /// <param name="symbol">string of symbol</param>
+        /// <returns>KuCoinOpenOrders object</returns>
+        Task<OpenOrderResponse> GetOpenOrders(string symbol);
+
+        /// <summary>
+        /// Get Order Book for a pair
+        /// </summary>
+        /// <param name="symbol">string of trading pair</param>
+        /// <param name="limit">number of orders to return per side, default 100</param>
+        /// <returns>OrderBook object</returns>
+        Task<OrderBookResponse> GetOrderBook(string symbol, int limit = 100);
+
+        /// <summary>
+        /// Post/Place a trade
+        /// </summary>
+        /// <param name="tradeParams">Trade to place</param>
+        /// <returns>KuCoinResponse object</returns>
+        Task<ApiResponse<Dictionary<string, string>>> PostTrade(TradeParams tradeParams);
+
+        /// <summary>
+        /// Delete/Cancel a trade
+        /// </summary>
+        /// <param name="symbol">Trading symbol</param>
+        /// <param name="orderOid">Order id to cancel</param>
+        /// <param name="tradeType">Trade type to cancel</param>
+        /// <returns>TradeResponse object</returns>
+        Task<DeleteResponse> DeleteTrade(string symbol, string orderOid, string tradeType);
+
+        /// <summary>
+        /// Get Ticker for all pairs
+        /// </summary>
+        /// <returns>Array of KuCoinTick objects</returns>
+        Task<Tick[]> GetTicks();
+
+        /// <summary>
+        /// Get Tick for a symbol
+        /// </summary>
+        /// <param name="symbol">Trading symbol</param>
+        /// <returns>KuCoinTick object</returns>
+        Task<Tick> GetTick(string symbol);
+
+        /// <summary>
+        /// Get KuCoinTime
+        /// </summary>
+        /// <returns>long of timestamp</returns>
+        Task<long> GetKuCoinTime();
+    }
+}
---
+++ KuCoinApi.NetCore/KuCoinApi.NetCore/Data/KuCoinRepository.cs
@@ -0,0 +1,425 @@
+﻿using KuCoinApi.NetCore.Entities;
+using KuCoinApi.NetCore.Core;
+using KuCoinApi.NetCore.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KuCoinApi.NetCore.Data
+{
+    public class KuCoinRepository : IKuCoinRepository
+    {
+        private Security security;
+        private IRESTRepository _restRepo;
+        private string baseUrl;
+        private ApiInformation _apiInfo;
+        private DateTimeHelper _dtHelper;
+        private Helper _helper;
+
+        /// <summary>
+        /// Constructor for non-signed endpoints
+        /// </summary>
+        public KuCoinRepository()
+        {
+            LoadRepository();
+        }
+
+        /// <summary>
+        /// Constructor for signed endpoints
+        /// </summary>
+        /// <param name="apiKey">Api key</param>
+        /// <param name="apiSecret">Api secret</param>
+        public KuCoinRepository(string apiKey, string apiSecret)
+        {
+            LoadRepository();
+        }
+
+        /// <summary>
+        /// Load repository
+        /// </summary>
+        /// <param name="key">Api key value (default = "")</param>
+        /// <param name="secret">Api secret value (default = "")</param>
+        private void LoadRepository(string key = "", string secret = "")
+        {
+            security = new Security();
+            _restRepo = new RESTRepository();
+            baseUrl = "https://api.kucoin.com";
+            _dtHelper = new DateTimeHelper();
+            _helper = new Helper();
+            _apiInfo = new ApiInformation
+            {
+                apiKey = key,
+                apiSecret = secret
+            };
+        }
+
+        /// <summary>
+        /// Check if the Exchange Repository is ready for trading
+        /// </summary>
+        /// <returns>Boolean of validation</returns>
+        public bool ValidateExchangeConfigured()
+        {
+            var ready = string.IsNullOrEmpty(_apiInfo.apiKey) ? false : true;
+            if (!ready)
+                return false;
+
+            return string.IsNullOrEmpty(_apiInfo.apiSecret) ? false : true;
+        }
+
+        /// <summary>
+        /// Get candlesticks
+        /// </summary>
+        /// <param name="symbol">String of symbol</param>
+        /// <param name="size">stick size</param>
+        /// <param name="limit">number of sticks</param>
+        /// <returns>ChartValue object</returns>
+        public async Task<ChartValue> GetCandlesticks(string symbol, Interval size, int limit)
+        {
+            var kuPair = _helper.CreateDashedPair(symbol);
+            var to = _dtHelper.UTCEndOfMinuteToUnixTime();// _dtHelper.UTCtoUnixTime();
+            var from = _helper.GetFromUnixTime(to, size, (limit + 2));
+            var kuSize = _helper.IntervalToKuCoinStringInterval(size);
+            var endpoint = $"/v1/open/chart/history?symbol={kuPair}&resolution={kuSize}&from={from}&to={to}";
+            var url = baseUrl + endpoint;
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ChartValue>(url);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get account balance
+        /// </summary>
+        /// <returns>Balance array</returns>
+        public async Task<Balance[]> GetBalance()
+        {
+            var endpoint = "/v1/account/balance";
+            var url = baseUrl + endpoint;
+
+            var headers = GetRequestHeaders(endpoint, null);
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<Balance[]>>(url, headers);
+
+                return response.data;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get order information
+        /// </summary>
+        /// <param name="symbol">string of symbol</param>
+        /// <param name="tradeType">Trade type</param>
+        /// <param name="orderId">long of orderId</param>
+        /// <param name="page">Page number, default 1</param>
+        /// <param name="limit">Number of fills to return, default 20</param>
+        /// <returns>OrderResponse object</returns>
+        public async Task<OrderListDetail> GetOrder(string symbol, TradeType tradeType, long orderId, int page = 1, int limit = 20)
+        {
+            var endpoint = "/v1/order/detail";
+            var url = baseUrl + endpoint;
+            var kuPair = _helper.CreateDashedPair(symbol);
+
+            var queryString = new List<string>
+            {
+                $"symbol={kuPair}",
+                $"type={tradeType.ToString()}",
+                $"limit={limit}",
+                $"page={page}",
+                $"orderOid={orderId}"
+            };
+
+            var headers = GetRequestHeaders(endpoint, queryString.ToArray());
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<DealOrder<OrderListDetail>>>(url, headers);
+
+                return response.data.datas;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get all current user order information
+        /// </summary>
+        /// <param name="symbol">string of symbol</param>
+        /// <param name="limit">Int of orders count to return, default 20</param>
+        /// <param name="page">Int of page number</param>
+        /// <returns>OpenOrderResponse object</returns>
+        public async Task<OrderListDetail[]> GetOrders(string symbol, int limit = 20, int page = 1)
+        {
+            var endpoint = "/v1/deal-orders";
+            var kuPair = _helper.CreateDashedPair(symbol);
+
+            var queryString = new List<string>
+            {
+                $"limit={limit}",
+                $"page={page}",
+                $"symbol={kuPair}"
+            };
+
+            var headers = GetRequestHeaders(endpoint, queryString.ToArray());
+
+            var url = baseUrl + endpoint + $"?{queryString[0]}&{queryString[1]}&{queryString[2]}";
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<DealOrder<OrderListDetail[]>>>(url, headers);
+
+                return response.data.datas;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get all open orders
+        /// </summary>
+        /// <param name="symbol">string of symbol</param>
+        /// <returns>KuCoinOpenOrders object</returns>
+        public async Task<OpenOrderResponse> GetOpenOrders(string symbol)
+        {
+            var endpoint = "/v1/order/active";
+            var kuPair = _helper.CreateDashedPair(symbol);
+
+            var queryString = new List<string>
+            {
+                $"symbol={kuPair}"
+            };
+
+            var headers = GetRequestHeaders(endpoint, queryString.ToArray());
+
+            var url = baseUrl + endpoint + $"?{queryString[0]}";
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<OpenOrderResponse>>(url, headers);
+            
+                return response.data;            
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get Order Book for a pair
+        /// </summary>
+        /// <param name="symbol">string of trading pair</param>
+        /// <param name="limit">number of orders to return per side, default 100</param>
+        /// <returns>OrderBook object</returns>
+        public async Task<OrderBookResponse> GetOrderBook(string symbol, int limit = 100)
+        {
+            var kuPair = _helper.CreateDashedPair(symbol);
+            var endpoint = $"/v1/open/orders?symbol={kuPair}&limit={limit}";
+            var url = baseUrl + endpoint;
+            
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<OrderBookResponse>>(url);
+
+                return response.data;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Post/Place a trade
+        /// </summary>
+        /// <param name="tradeParams">Trade to place</param>
+        /// <returns>KuCoinResponse object</returns>
+        public async Task<ApiResponse<Dictionary<string, string>>> PostTrade(TradeParams tradeParams)
+        {
+            var endpoint = $"/v1/order";
+            var kuPair = _helper.CreateDashedPair(tradeParams.symbol);
+
+            var queryString = new List<string>
+            {
+                $"symbol={kuPair}",
+                $"amount={tradeParams.quantity}",
+                $"price={tradeParams.price}",
+                $"type={tradeParams.side}"
+            };
+
+            var headers = GetRequestHeaders(endpoint, queryString.ToArray());
+
+            var url = baseUrl + endpoint + $"?{_helper.ArrayToString(queryString.ToArray())}";
+
+            try
+            {
+                var response = await _restRepo.PostApi<ApiResponse<Dictionary<string, string>>>(url, headers);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Delete/Cancel a trade
+        /// </summary>
+        /// <param name="symbol">Trading symbol</param>
+        /// <param name="orderOid">Order id to cancel</param>
+        /// <param name="tradeType">Trade type to cancel</param>
+        /// <returns>TradeResponse object</returns>
+        public async Task<DeleteResponse> DeleteTrade(string symbol, string orderOid, string tradeType)
+        {
+            var endpoint = "/v1/cancel-order";
+            var kuPair = _helper.CreateDashedPair(symbol);
+
+            var queryString = new List<string>
+            {
+                $"symbol={kuPair}",
+                $"orderOid={orderOid}",
+                $"type={tradeType}"
+            };
+
+            var headers = GetRequestHeaders(endpoint, queryString.ToArray());
+
+            var url = baseUrl + endpoint + $"?{_helper.ArrayToString(queryString.ToArray())}";
+
+            try
+            {
+                var response = await _restRepo.PostApi<DeleteResponse>(url, headers);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get Ticker for all pairs
+        /// </summary>
+        /// <returns>Array of KuCoinTick objects</returns>
+        public async Task<Tick[]> GetTicks()
+        {
+            var endpoint = "/v1/open/tick";
+            var url = baseUrl + endpoint;
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<Tick[]>>(url);
+
+                return response.data;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get Tick for a symbol
+        /// </summary>
+        /// <param name="symbol">Trading symbol</param>
+        /// <returns>KuCoinTick object</returns>
+        public async Task<Tick> GetTick(string symbol)
+        {
+            var kuPair = _helper.CreateDashedPair(symbol);
+            var endpoint = $"/v1/open/tick?symbol={kuPair}";
+            var url = baseUrl + endpoint;
+
+            try
+            {
+                var response = await _restRepo.GetApiStream<ApiResponse<Tick>>(url);
+
+                return response.data;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get KuCoinTime
+        /// </summary>
+        /// <returns>long of timestamp</returns>
+        public async Task<long> GetKuCoinTime()
+        {
+            var endpoint = "/v1/time";
+            var url = baseUrl + endpoint;
+
+            var response = await _restRepo.GetApi<long>(url);
+
+            return response;
+        }
+
+        private Dictionary<string, string> GetRequestHeaders(string endpoint, string[] queryString = null)
+        {
+            var nonce = _dtHelper.UTCtoUnixTimeMilliseconds().ToString();
+            var headers = new Dictionary<string, string>();
+
+            headers.Add("KC-API-KEY", _apiInfo.apiKey);
+            headers.Add("KC-API-NONCE", nonce);
+            headers.Add("KC-API-SIGNATURE", GetSignature(endpoint, nonce, queryString, 0));
+            headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+
+            return headers;
+        }
+
+        private Dictionary<string, string> PostRequestHeaders<T>(string endpoint, string[] queryString, T postData)
+        {
+            var nonce = _dtHelper.UTCtoUnixTimeMilliseconds().ToString();
+            var headers = new Dictionary<string, string>();
+
+            headers.Add("KC-API-KEY", _apiInfo.apiKey);
+            headers.Add("KC-API-NONCE", nonce);
+            headers.Add("KC-API-SIGNATURE", GetSignature(endpoint, nonce, queryString, postData));
+            headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+
+            return headers;
+        }
+        
+        private string GetSignature<T>(string endpoint, string nonce, string[] queryString = null, T value = default(T))
+        {
+            queryString = queryString ?? new string[0];
+
+            Array.Sort(queryString, StringComparer.InvariantCulture);
+
+            var qsValues = string.Empty;
+
+            if(queryString.Length> 0)
+            {
+                qsValues = _helper.ArrayToString(queryString);
+            }
+            else if(typeof(T) != typeof(int))
+            {
+                qsValues += _helper.ObjectToString<T>(value);
+            }
+
+            var sigString = $"{endpoint}/{nonce}/{qsValues}";
+
+            var signature = security.GetKuCoinHMCACSignature(_apiInfo.apiSecret, sigString);
+
+            return signature;
+        }
+    }
+}
